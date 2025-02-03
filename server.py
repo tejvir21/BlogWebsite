fab51171eda99ffc922304f48e856f0428f81983
Tejvir Chauhan <tejvir.c786@gmail.com>
2025-02-03 11:15:56 +0530

    Update server.py

--- server.py
+++ server.py
@@ -257,7 +257,7 @@ def send_email(name, email, phone, message):
         connection.login(user=MAIL_ADDRESS, password=MAIL_APP_PW)
         connection.sendmail(from_addr=MAIL_ADDRESS, to_addrs=MAIL_ADDRESS_RECEIVER, msg=email_message)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=True)
     # app.run(debug=True)
